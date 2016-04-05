e397c42f0a268e6b59c41910a5f79232f12c1854
Karen Quan <quan.karen@gmail.com>
2016-04-04 20:52:49 -0700

    display custom palette lists on palette page

--- public/js/app/palettes/palettes.controller.js
+++ public/js/app/palettes/palettes.controller.js
@@ -5,20 +5,29 @@
     .module("app")
     .controller("PalettesController", PalettesController);
 
-  PalettesController.$inject = ["$log", "$http", "palettesService", "tokenService"];
+  PalettesController.$inject = ["$log", "$http", "palettesService", "tokenService", "authService"];
 
-  function PalettesController($log, $http, palettesService, token) {
+  function PalettesController($log, $http, palettesService, token, authService) {
     var vm = this;
 
     // BINDINGS
+    vm.currentUserPaletteLists;
     vm.getCurrentUserPaletteLists;
     vm.palette;
 
     getPalette();
+    getCurrentUserPaletteLists();
 
     // FUNCTIONS
     function getCurrentUserPaletteLists() {
-
+      authService.currentUser()
+      .then(function(response) {
+        $log.info(response);
+        var user = response;
+        vm.currentUserPaletteLists = user.paletteLists;
+      }, function(error) {
+        $log.error(error);
+      });
     }
 
     function getPalette() {
